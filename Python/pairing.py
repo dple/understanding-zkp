18d7466b8eb56a9fa962ebe41cbb3c4489b89fa8
Le Duc Phong <le.duc.phong@gmail.com>
2024-02-28 15:29:21 -0500

    Moved to Python folder

--- Python/pairing.py
+++ Python/pairing.py
@@ -10,9 +10,13 @@ if __name__ == '__main__':
     that is the first argument of pairing will be the element, Q, in the extension field F_pˆ12, 
     and the second argument, P, will be in the base field F_p   
     """
-    print(pairing(Q, P))
-    T = multiply(G2, 6)
+    print(pairing(Q, P))        # The result will be an element in the extension field, F_pˆ12,
+                                # representing by 12 element in the base field.
+                                # The computation takes a few seconds, pretty slow,
+                                # perhaps because the implementation of pairing in py-ecc is not optimized
+
     # As pairing(a*G2, b*G1) == pairing(a*b*G2, G1), the following return true
+    T = multiply(G2, 6)
     print(eq(pairing(Q, P), pairing(T, G1)))
-    # And this will print 1 as e(G2, G2)ˆ{-ab} * e(G2, G2)ˆ{ab} = 1
+    # And this will print 1 as e(G2, G2)ˆ{-ab} * e(G2, G2)ˆ{ab} = e(G2, G1)ˆ0 = 1
     print(pairing(neg(Q), P) * pairing(T, G1))
