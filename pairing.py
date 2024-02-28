ddf8b58c31842fb7004146f1edef75bee45901ec
Le Duc Phong <le.duc.phong@gmail.com>
2024-02-27 22:48:34 -0500

    Update pairing.py

--- pairing.py
+++ pairing.py
@@ -4,8 +4,13 @@ if __name__ == '__main__':
     print(G1)
     print(G2)
     P = multiply(G1, 3)
-    Q = multiply(G2, 2)
+    Q = add(G2, G2)
+    """
+    py-ecc library implemented Ate pairing, that switches the order of P & Q, 
+    that is the first argument of pairing will be the element, Q, in the extension field F_pˆ12, 
+    and the second argument, P, will be in the base field F_p   
+    """
     print(pairing(Q, P))
-    A = multiply(G2, 5)
-    B = multiply(G1, 6)
-    print(pairing(A, B))
+    T = multiply(G2, 6)
+    # As pairing(a*G2, b*G1) == pairing(a*b*G2, G1), the following return true
+    print(eq(pairing(Q, P), pairing(T, G1)))
