7ef0c7c06ac99b3132d0ec3ab45af696ed411824
Le Duc Phong <le.duc.phong@gmail.com>
2024-03-09 12:49:16 -0500

    Create enc_poly_eval.py

---
+++ Python/enc_poly_eval.py
@@ -0,0 +1,9 @@
+"""
+This Python code was consolidated from the following lecture by Rareskills
+                Encrypted Polynomial Evaluation
+(https://www.rareskills.io/post/encrypted-polynomial-evaluation)
+"""
+import numpy as np
+import galois
+from utils import *
+
